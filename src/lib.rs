685fda425c614573e633956bc9083771319e0301
Brent Bonet <brent@brentbonet.com>
2023-05-10 09:07:58 -0600

    14-254

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
     log(&"Grayscale called".into());
     let base64_to_vector = decode(encoded_file).unwrap();
-    log(&"Image decoded".into())
+    log(&"Image decoded".into());
+
+    let img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
 }
