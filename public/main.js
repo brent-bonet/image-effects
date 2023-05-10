14c27866c3185817333cc65173cdfada9f3cd6b5
Brent Bonet <brent@brentbonet.com>
2023-05-10 08:52:17 -0600

    14-252

--- public/main.js
+++ public/main.js
@@ -1,28 +1,29 @@
 async function init() {
-    let rustApp = null;
-    try{
-        rustApp = await import('../pkg');
-    } catch (e) {
-        console.error(e);
-        return;
-    }
+  let rustApp = null;
+  try {
+    rustApp = await import("../pkg");
+  } catch (e) {
+    console.error(e);
+    return;
+  }
 
-    console.log(rustApp);
+  console.log(rustApp);
 
-    const input = document.getElementById('upload') ;
-    const fileReader = new FileReader();
+  const input = document.getElementById("upload");
+  const fileReader = new FileReader();
 
-     fileReader.onloadend = () => {
-        const base64 = fileReader.result.replace(
-            /^data:image\/(png|jpeg|jpg);base64,/, ''
-        );
-        console.log(input.files[0]);
-        console.log(base64);
-     };
+  fileReader.onloadend = () => {
+    const base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+    // console.log(input.files[0]);
+    rustApp.grayscale(base64);
+  };
 
-    input.addEventListener('change', () => {
-        fileReader.readAsDataURL(input.files[0]);
-    });
+  input.addEventListener("change", () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
 }
 
 init();
